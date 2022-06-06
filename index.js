80a115dd377b486e0d9d2b0649bf1728ade03f88
Judy Mwai <juddiemukami@gmail.com>
2022-06-07 00:07:19 +0300

    commit tree 80129a15bcc4c84f074141b0208a10e2f871966d

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 
 // Write your code here
-const num1;
-const num2;
-function multiply(num1, num2){
-    return num1*num2 ==62;
-}
+let num1 = 1;
+let num2 = 62;
+let multiply= (num1 * num2)
+
+
+
+let random = Math.ceil(Math.random()*10)
+console.log(random)
+
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4
+
+
+let max = Math.max(20, 7, 9, 2, 19) 
+console.log(max)
